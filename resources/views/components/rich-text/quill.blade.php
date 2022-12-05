64f525f7f80cd80bee86b694f36b44d9f8c4bd92
Philipp Bürger <69788981+philippbuerger@users.noreply.github.com>
2022-12-05 14:52:11 +0100

    Update quill.blade.php

--- resources/views/components/rich-text/quill.blade.php
+++ resources/views/components/rich-text/quill.blade.php
@@ -1,13 +1,39 @@
 @props([
     'name',
-    'required' => false
+    'required' => false,
+    'toolbar' => true
 ])
 
 <div
     x-data="{
         value: '',
         init() {
-            let quill = new Quill(this.$refs.quill, { theme: 'snow' })
+
+            @if ($toolbar)
+                var toolbarOptions = [
+                    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+                    ['blockquote', 'code-block'],
+
+                    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+                    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+                    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+                    [{ 'direction': 'rtl' }],                         // text direction
+
+                    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+                    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+                    [{ 'font': [] }],
+                    [{ 'align': [] }],
+
+                    ['clean']                                         // remove formatting button
+                ];
+            @else
+                var toolbarOptions = false
+            @endif
+
+            let quill = new Quill(this.$refs.quill, { theme: 'snow', modules: { toolbar: toolbarOptions } })
 
             quill.root.innerHTML = this.value
 
